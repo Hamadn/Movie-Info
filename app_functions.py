8350ad21f75398a8a3a9e3c49e0e1371479e32fd
Hamadn <nhamad919@gmail.com>
2024-01-17 14:43:16 +0400

    Added a function to show similar movies and added error handling

--- app_functions.py
+++ app_functions.py
@@ -55,3 +55,40 @@ class MovieFunctions:
         except Exception as e:
             tk.messagebox.showerror(
                 "Error", f"An unexpected error occurred: Please double check the spelling of the movie name and try again.")
+
+    # Define a function to show similar movies
+    def show_similar_movies(self, movie_id):
+        try:
+            # Get similar movies
+            self.similar_movies_data = self.movie.similar(movie_id)
+
+            # Clear previous search results and display year range filter
+            for widget in self.scrollable_frame.winfo_children():
+                widget.destroy()
+            self.display_year_range_filter()
+
+            # Debugging output
+            print(f"Number of similar movies found: {
+                  len(self.similar_movies_data)}")
+
+            # Display count of all similar movies found at row=1
+            count_label = ttk.Label(self.scrollable_frame, text=f"Number of similar movies found: {
+                len(self.similar_movies_data)}")
+            count_label.grid(row=1, column=0, sticky='w', pady=(10, 0))
+
+            # Start displaying movies from row=2
+            movie_row_index = 2
+            for m in self.similar_movies_data:
+                # Display each movie
+                self.display_movie(self.scrollable_frame, m, movie_row_index)
+                movie_row_index += 1
+
+            # Refresh the window (optional, try if label is still not visible)
+            self.scrollable_frame.update_idletasks()
+
+            # Display the similar movies
+            self.display_similar_movies(self.similar_movies_data)
+        # Handle request exceptions
+        except requests.exceptions.RequestException as e:
+            # Show an error message if a request exception occurs
+            tk.messagebox.showerror("Error", f"An error occurred: {e}")
